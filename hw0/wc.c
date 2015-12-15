494e744cb05fbc5243a70e2fe03e0390171995c8
Jorge Guevara <jlguevara81@gmail.com>
2015-12-14 23:33:22 -0800

    separated stdin and finename inputs

--- hw0/wc.c
+++ hw0/wc.c
@@ -1,3 +1,26 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+
 int main(int argc, char *argv[]) {
-    return 0;
+	FILE *f;
+
+	if (argc == 2) {
+		char *filename = argv[1];
+
+		f = fopen(filename, "r");
+		if (f == NULL) {
+			printf("errno: %d\n", errno);
+			perror(filename);
+			exit(1);
+		}
+	}
+	else if (argc == 1) {
+		f = stdin;
+	}
+
+	int input;
+	while ((input = fgetc(f)) != EOF)
+		printf("%c\n", input);
 }
